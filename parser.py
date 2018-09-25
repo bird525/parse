83d480eec38df99c1af23a8d256e6540b089123c
bird525 <bird525@nextmail.ru>
2018-09-25 14:55:59 +0300

    Add function "base_write", who write item name and description in table "tea"

--- parser.py
+++ parser.py
@@ -2,6 +2,14 @@ import requests
 import os
 from bs4 import BeautifulSoup
 #import re
+import sqlite3
+
+def base_write(name, description):
+	connection = sqlite3.connect('D:\\moychay\\base\\products.db')
+	cursor = connection.cursor()
+	cursor.execute('''INSERT INTO tea (name, description) VALUES (?,?)''', (name, description))
+	connection.commit()
+	connection.close()
 
 def catalog_page_parser(url):
 	page = requests.get(url)
@@ -18,14 +26,19 @@ def item_parser(url):
 	soup = BeautifulSoup(page.text, 'html.parser')
 	item_name = soup.find_all('h1', itemprop = 'name')[0].get_text()
 	item_description = soup.find('article', class_ = 'item-description').find('div', class_ = 'description').find_all('p')
-	f = open('D:\\moychay\\base\\result.txt', 'a', encoding = 'utf-8')
-	f.write('\n' + item_name + '\n')
+	#f = open('D:\\moychay\\base\\result.txt', 'a', encoding = 'utf-8')
+	#f.write('\n' + item_name + '\n')
 	s = ''
 	for item in item_description:
 		s += item.text.replace('\n', '')
 	#result = re.split(r'(?<=\w[.!?]) ', s)        #Разбивает текст по точкам на предложения, не учитывает г. итд
-	f.write(s)
-	f.close()
+	#f.write(s)
+	#f.close()
+	base_write(item_name, s)
+	
+
+
+
 	'''item_images = soup.find_all('a', class_ = 'popup')
 	for img in item_images:
 		u = 'http://rostovondon.moychay.ru' + img.get('href')
