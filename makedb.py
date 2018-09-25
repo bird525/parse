9e839bd7cece5342dfcb7b81cfd844744d9dd0e7
bird525 <bird525@nextmail.ru>
2018-09-25 14:53:46 +0300

    Add table with photos

--- makedb.py
+++ makedb.py
@@ -1,5 +1,11 @@
 import sqlite3
 connection = sqlite3.connect('D:\\moychay\\base\\products.db')
 cursor = connection.cursor()
+cursor.execute('''CREATE TABLE tea
+				  (id integer primary key autoincrement, name text, description text)
+			   ''')
+cursor.execute('''CREATE TABLE tea_photos
+				  (name text, ref text, item_id integer, FOREIGN KEY(item_id) REFERENCES tea(id))''')
+connection.commit()
 connection.close()
 print('Woow!')
